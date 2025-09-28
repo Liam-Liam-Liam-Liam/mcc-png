4aeed2e7e69a8961e7a2067d2bcd2aa7ab97f91f
Liam-Liam-Liam-Liam <liammccolgan02@gmail.com>
2025-09-27 22:17:19 -0400

    Uploaded Albums To Start
    Uploaded 4 albums and did a bunch of shit

---
+++ albums.js
@@ -0,0 +1,27 @@
+// albums.js
+window.ALBUMS = [
+  {
+    title: "Gee-Gees Rowing",
+    href: "album_geegees_rowing.html",
+    coverId: "1TsaUa048ll1rN31Mu3MROSU2meFkNq7d",
+
+  },
+  {
+    title: "Ottawa Titans",
+    href: "album_ottawa_titans.html",
+    coverId: "1PGM_T9rdeTBcbDxaPDmsfDk-3qzD1Jv1",
+
+  },
+  {
+    title: "Sensplex Men's Hockey",
+    href: "album_sensplex_hockey_mens.html",
+    coverId: "1n--Q2gQvKoJVjw7w6134hFvsAxDlaH9M",
+
+  },
+  {
+    title: "Sensplex Women’s Hockey ",
+    href: "album_sensplex_hockey_womens.html",
+    coverId: "1v0XXzh2rbWshK8xyp_h-a_R7PZNJd_mO",
+
+  }
+];
